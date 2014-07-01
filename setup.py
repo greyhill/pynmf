94b26dbb5f0a8f9bdeb5d3bc8a78467d6ba952f1
Madison McGaffin <greyhill@gmail.com>
2014-07-01 12:02:43 -0400

    added distutils boilerplate

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+        name = 'pynmf',
+        version = '0.0.1',
+        author = 'Madison McGaffin',
+        author_email = 'greyhill@gmail.com',
+        packages = [ 'nmf' ],
+        url = 'http://github.com/greyhill/pynmf',
+        description = 'Nonnegative matrix factorization',
+        install_requires = [ 
+            'numpy'
+        ]
+    )
+
