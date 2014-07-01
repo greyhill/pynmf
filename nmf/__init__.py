78fc93b58b7ebc91df7fb926ced63b8b15bf42b9
Madison McGaffin <greyhill@gmail.com>
2014-07-01 12:10:19 -0400

    very simple nmf implementation

---
+++ nmf/__init__.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def improve(H, C, D):
+    '''
+    Improve the nonnegative approximation H ~ CD
+
+    The user is responsible for initializing C and D with nonnegative entries.
+    Because this is a multiplicative algorithm, any sparsity in C and D is
+    preserved.
+
+    '''
+    # D update
+    D = D * C.dot(H) / C.dot(C).dot(H)
+
+    # C update
+    C = C * H.dot(D.t) / C.dot(D).dot(D.t)
+
+    return C, D
+
